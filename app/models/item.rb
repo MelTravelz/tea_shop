bbb284af503b3b412f2faf0d390759c60fdf1fad
Melony Franchini <meltravelz@gmail.com>
2023-03-27 17:14:30 -0600

    Add first merchant/index request test & controller

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   validates_presence_of :name 
   validates_presence_of :description
   validates_presence_of :unit_price
+  validates_numericality_of :unit_price
   validates_presence_of :merchant_id
   
   belongs_to :merchant
