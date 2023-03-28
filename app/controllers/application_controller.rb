f4cd5032e1d16239aded5090f3ad8093ce511806
Melony Franchini <meltravelz@gmail.com>
2023-03-27 22:11:26 -0600

    Add ApplicationController#rescue_from

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # we add this "call back" to rescue the error and call on the method below
+  # so the error message is customized
+
+  def render_not_found_response(exception)
+    render json: { message: exception.message }, status: :not_found
+  end
 end
