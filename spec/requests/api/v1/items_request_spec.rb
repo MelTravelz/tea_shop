9619a9dab5fd303a012bc0fd2d46080e98e8e012
Melony Franchini <meltravelz@gmail.com>
2023-03-27 23:37:03 -0600

    Create ItemsController#show happy path test

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "Items API" do
+  let(:first_item) { Item.first }
+
   before do
     create_list(:item, 5)
   end
@@ -21,10 +23,38 @@ RSpec.describe "Items API" do
         expect(parsed_data[:data][0].keys).to eq([:id, :type, :attributes])
 
         expect(parsed_data[:data][0][:attributes].keys).to eq([:name, :description, :unit_price, :merchant_id])
-        expect(parsed_data[:data][0][:attributes][:name]).to eq(Item.first.name)
-        expect(parsed_data[:data][0][:attributes][:description]).to eq(Item.first.description)
-        expect(parsed_data[:data][0][:attributes][:unit_price]).to eq(Item.first.unit_price)
+        expect(parsed_data[:data][0][:attributes][:name]).to eq(first_item.name)
+        expect(parsed_data[:data][0][:attributes][:description]).to eq(first_item.description)
+        expect(parsed_data[:data][0][:attributes][:unit_price]).to eq(first_item.unit_price)
         expect(parsed_data[:data][0][:attributes][:unit_price]).to be_a(Float)
+      end
+    end
+  end
+
+  describe "#show" do
+    context "when successful" do
+      before do
+        get "/api/v1/items/#{first_item.id}"
+      end
+
+      it "returns one item" do
+        expect(response).to be_successful
+        
+        parsed_data = JSON.parse(response.body, symbolize_names: true)
+
+        expect(parsed_data.size).to eq(1)
+
+        expect(parsed_data[:data].keys).to eq([:id, :type, :attributes])
+        expect(parsed_data[:data][:attributes].keys).to eq([:name, :description, :unit_price, :merchant_id])
+
+
+        expect(parsed_data[:data][:id]).to eq(first_item.id.to_s)
+        expect(parsed_data[:data][:type]).to eq('item')
+
+        expect(parsed_data[:data][:attributes][:name]).to eq(first_item.name)
+        expect(parsed_data[:data][:attributes][:description]).to eq(first_item.description)
+        expect(parsed_data[:data][:attributes][:unit_price]).to eq(first_item.unit_price)
+        expect(parsed_data[:data][:attributes][:merchant_id]).to eq(first_item.merchant_id)
       end
     end
   end
