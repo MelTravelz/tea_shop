20e702a9c7b0c039ed8711b98e03cf745afdd262
Melony Franchini <meltravelz@gmail.com>
2023-03-28 13:01:05 -0600

    Update item validations & tests

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,9 @@
 class Item < ApplicationRecord
   validates :name, presence: true #, length: { maximum: 20 }
-  # validates :description, presence: true  <- NOT ABSOLUTELY NECESSARY
+  validates :description, presence: true 
   validates :unit_price, presence: true, numericality: true
+  validates :merchant_id, presence: true, numericality: true
+
   
   belongs_to :merchant
 end
