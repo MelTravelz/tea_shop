96b5ec8d70e4cf8e9c0d600b3622764bc6b1235a
Melony Franchini <meltravelz@gmail.com>
2023-03-30 23:41:59 -0600

    Updated `Item`
    Created `::find_items_by_term`
    Created `::find_item_by_price`

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -48,4 +48,41 @@ RSpec.describe Item, type: :model do
     end
   end
 
+  describe "class methods" do
+    before do
+      merchant = create(:merchant)
+
+      @honey = create(:item, merchant: merchant, name: "Honey Vanilla", unit_price: 5.00)
+      @sleepy_mint = create(:item, merchant: merchant, name: "Sleepytime Mint", unit_price: 8.55)
+      @sleepy_relaxy = create(:item, merchant: merchant, name: "Sleepy Relaxy", unit_price: 10.00)
+      @tension_tamer = create(:item, merchant: merchant, name: "Tension Tamer", unit_price: 12.35)
+      @sleepy = create(:item, merchant: merchant, name: "Sleepytime", unit_price: 3.25)
+    end
+
+    describe "::find_items_by_term" do
+      it "returns case-insensitive, alphabetical matches when search term is found" do
+        expect(Item.find_items_by_term("slEEpy")).to eq([@sleepy_relaxy, @sleepy, @sleepy_mint])
+        
+        expect(Item.find_items_by_term("slEEpy")).to_not eq([@honey, @sleepy_relaxy, @sleepy, @sleepy_mint, @tension_tamer])
+      end
+
+      it "returns [] when search term is NOT found" do
+        expect(Item.find_items_by_term("xyz")).to eq([])
+      end
+    end
+
+    describe "::find_items_by_price" do
+      it "returns only items BETWEEN min/max price" do
+        expect(Item.find_items_by_price(5.00, 10.00)).to eq([@honey, @sleepy_mint, @sleepy_relaxy])
+
+        expect(Item.find_items_by_price(0, 5.00)).to eq([@sleepy, @honey])
+
+        expect(Item.find_items_by_price(10.00, 100.00)).to eq([@sleepy_relaxy, @tension_tamer])
+      end
+
+      it "returns [] when min_price is so big NO item it found" do
+        expect(Item.find_items_by_price(20.00, 50.00)).to eq([])
+      end
+    end
+  end
 end
